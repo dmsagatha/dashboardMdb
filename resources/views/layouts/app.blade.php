c681b62cb41662097e02c3edad7d74cd51b3acc7
Doris Morales <dmorales55@gmail.com>
2021-02-13 00:23:12 -0500

    SideNavLeft sin jQuery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -168,20 +168,23 @@
     <script>
       let openBtn = document.querySelector(".openSideNav");
       openBtn.addEventListener("click", () => {
-      showNav();
+        showNav();
       });
+
       let closeBtn = document.querySelector(".closeBtn");
-      closeBtn.addEventListener("click", () => {
-      hideNav();
+        closeBtn.addEventListener("click", () => {
+          hideNav();
       });
+
       function showNav() {
-      document.querySelector(".sideNav").style.width = "300px";
-      document.querySelector('.main-content').style.marginLeft = "300px";
+        document.querySelector(".sideNav").style.width = "300px";
+        document.querySelector('.main-content').style.marginLeft = "300px";
       }
+
       function hideNav() {
-      document.querySelector(".sideNav").style.width = "0";
-      document.querySelector('.main-content').style.marginLeft = "0px";
+        document.querySelector(".sideNav").style.width = "0";
+        document.querySelector('.main-content').style.marginLeft = "0px";
       }
-      </script>
+    </script>
   </body>
 </html>
