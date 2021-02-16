254a33d6dc9d1ad454c9af6382948fac91a7e173
Doris Morales <dmorales55@gmail.com>
2021-02-15 23:26:33 -0500

    SideNavLeft con JavaScript

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,115 +16,29 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    <style>
-      .navbar-brand {
-        font-family: 'Font Awesome 5 Free';
-        font-weight: 700;
-        font-size: 1.5rem;
-        cursor: pointer;
-      }
-      
-      .navbar-brand:before {
-        content: "\f03a";
-      }
-
-      .main-content {
-        transition: 0.5s;
-      }
-
-      .preload {
-        transition: none !important;
-      }
-      
-      .nav__links {
-        position: fixed;
-        top: 60px;
-        left: 0;
-        z-index: 2;
-        height: 100vh;
-        width: 250px;
-        background: #ffffff;
-        transform: translateX(-250px);
-        transition: transform 0.3s;
-      }
-
-      .nav--open .nav__links {
-        transform: translateX(0);
-        box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
-      }
-
-      .nav__link {
-        display: flex;
-        align-items: center;
-        color: #666666;
-        font-weight: bold;
-        font-size: 14px;
-        text-decoration: none;
-        padding: 12px 15px;
-        background: transform 0.2s;
-      }
-
-      .nav__link > i {
-        margin-right: 15px;
-      }
-
-      .nav__link--active {
-        color: #009578;
-      }
-
-      .nav__link--active,
-      .nav__link:hover {
-        background: #eeeeee;
-      }
-
-      .overlay {
-        position: fixed;  /* Parte superior del contenido de la página */
-        top: 0;
-        left: 0;
-        width: 100vw;      /* Ancho completo o 100% */
-        height: 100vh;     /* Altura completa o 100% */
-        background-color: rgba(0,0,0,0.5); /* Fondo negro con opacidad */
-        backdrop-filter: blur(2px);
-        visibility: hidden;   /* Oculto por defecto */
-        opacity: 0;
-        z-index: 1; /* Orden de pila en caso de que esté usando un orden diferente para otros elementos */
-        cursor: pointer; /* Puntero al pasar el mouse */
-        /* transition: opacity 0.3s; */
-        transition: all 0.5s ease-in-out;
-      }
-
-      .nav--open .overlay {
-        visibility: visible;
-        opacity: 1;
-      }
-    </style>
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
   </head>
   <body class="preload">
     <header>
       @include('layouts.includes.templates._header')
     </header>
     
-    <nav class="nav">
-      <div class="nav__links">
-        <a href="#" class="nav__link">
-          <i class="fa fa-user"></i>Dashboard
-        </a>
-        <a class="nav__link nav__link--active" href="#">
-          <i class="fa fa-user"></i>Projects
-        </a>
-        <a class="nav__link" href="#">
-          <i class="fa fa-user"></i>Security
-        </a>
-        <a class="nav__link" href="#">
-          <i class="fa fa-user"></i>History
-        </a>
-        <a class="nav__link" href="#">
-          <i class="fa fa-user"></i>Profile
-        </a>
-      </div>
-      <div class="overlay"></div>
-    </nav>
+    <input type="checkbox" id="check">
+    <label for="check">
+      <i class="fas fa-bars" id="btn"></i>
+      <i class="fas fa-times" id="cancel"></i>
+    </label>
+
+    <div class="sidebar">
+      <header>Mi Menú</header>
+      <a href="#" class="active"><i class="fas fa-qrcode"></i><span>Dashboard</span></a>
+      <a href="#"><i class="fas fa-link"></i><span>Inicio</span></a>
+      <a href="#"><i class="fas fa-stream"></i><span>Usuarios</span></a>
+      <a href="#"><i class="fas fa-calendar"></i><span>Productos</span></a>
+      <a href="#"><i class="far fa-question-circle"></i><span>Pedidos</span></a>
+      <a href="#"><i class="fas fa-sliders-h"></i><span>Facturas</span></a>
+      <a href="#"><i class="far fa-envelope"></i><span>Configuraciones</span></a>
+    </div>
 
     <main class="main-content mt-5">
       <div class="container">  
@@ -136,6 +50,7 @@
             <h1>Esta es la primera página</h1>
             <hr>
             <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nesciunt aperiam assumenda dolores expedita autem veritatis distinctio vitae, dignissimos optio vel, quidem corrupti corporis aspernatur a. Excepturi dolore itaque illo.
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nesciunt aperiam assumenda dolores expedita autem veritatis distinctio vitae, dignissimos optio vel, quidem corrupti corporis aspernatur a. Excepturi dolore itaque illo.
             </p>
           </div>
@@ -213,24 +128,5 @@
 
     <!-- JavaScript -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- https://www.youtube.com/watch?v=yH7gkUNwDJY -->
-    <script>
-      window.addEventListener("load", () => {
-        document.body.classList.remove("preload");
-      });
-      
-      document.addEventListener("DOMContentLoaded", () => {
-        const nav = document.querySelector(".nav");
-
-        document.querySelector("#btnNav").addEventListener("click", () => {
-          nav.classList.add("nav--open");
-        });
-
-        document.querySelector(".overlay").addEventListener("click", () => {
-          nav.classList.remove("nav--open");
-        });
-      });
-    </script>
   </body>
 </html>
--- resources/views/layouts/app1.blade.php
+++ resources/views/layouts/app1.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
@@ -10,216 +9,228 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ asset('img/favicon.ico') }}">
 
-    <title>{{ config('app.name', 'Laravel SideNav') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     
-    <!-- Google Fonts Roboto -->
+    <!-- Google Fonts Roboto --><!-- Font Awesome -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <style>
-      .active-nav {
-        margin-left: 0;
+      .navbar-brand {
+        font-family: 'Font Awesome 5 Free';
+        font-weight: 700;
+        font-size: 1.5rem;
+        cursor: pointer;
+      }
+      
+      .navbar-brand:before {
+        content: "\f03a";
       }
 
-      .active-cont {
-        margin-left: 250px;
+      .main-content {
+        transition: 0.5s;
+      }
+
+      .preload {
+        transition: none !important;
+      }
+      
+      .nav__links {
+        position: fixed;
+        top: 60px;
+        left: 0;
+        z-index: 2;
+        height: 100vh;
+        width: 250px;
+        background: #ffffff;
+        transform: translateX(-250px);
+        transition: transform 0.3s;
+      }
+
+      .nav--open .nav__links {
+        transform: translateX(0);
+        box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+      }
+
+      .nav__link {
+        display: flex;
+        align-items: center;
+        color: #666666;
+        font-weight: bold;
+        font-size: 14px;
+        text-decoration: none;
+        padding: 12px 15px;
+        background: transform 0.2s;
+      }
+
+      .nav__link > i {
+        margin-right: 15px;
+      }
+
+      .nav__link--active {
+        color: #009578;
+      }
+
+      .nav__link--active,
+      .nav__link:hover {
+        background: #eeeeee;
+      }
+
+      .overlay {
+        position: fixed;  /* Parte superior del contenido de la página */
+        top: 0;
+        left: 0;
+        width: 100vw;      /* Ancho completo o 100% */
+        height: 100vh;     /* Altura completa o 100% */
+        background-color: rgba(0,0,0,0.5); /* Fondo negro con opacidad */
+        backdrop-filter: blur(2px);
+        visibility: hidden;   /* Oculto por defecto */
+        opacity: 0;
+        z-index: 1; /* Orden de pila en caso de que esté usando un orden diferente para otros elementos */
+        cursor: pointer; /* Puntero al pasar el mouse */
+        /* transition: opacity 0.3s; */
+        transition: all 0.5s ease-in-out;
+      }
+
+      .nav--open .overlay {
+        visibility: visible;
+        opacity: 1;
       }
     </style>
   </head>
-  <body>
+  <body class="preload">
     <header>
       @include('layouts.includes.templates._header')
     </header>
-
-    {{-- <nav id="sidebarNav" class="navbar navbar-expand d-flex flex-column align-items-start">
-      <a href="#" class="navbar-brand text-light mt-5">
-        <div class="display-5 font-weight-bold">SideNav</div>
-      </a>
-      <ul class="navbar-nav d-flex flex-column mt-5 w-100">
-        <li class="nav-item w-100">
-          <a href="#" class="nav-link text-light pl-4">Inicio</a>
-        </li>
-      </ul>
-    </nav> --}}
-
-    <div class="container-fluid my-container">
-      <div class="row">
-        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-          <div class="position-sticky pt-3">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  <span data-feather="home"></span>
-                  Dashboard
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file"></span>
-                  Orders
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="shopping-cart"></span>
-                  Products
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="users"></span>
-                  Customers
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="bar-chart-2"></span>
-                  Reports
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="layers"></span>
-                  Integrations
-                </a>
-              </li>
-            </ul>
-
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-              <span>Saved reports</span>
-              <a class="link-secondary" href="#" aria-label="Add a new report">
-                <span data-feather="plus-circle"></span>
-              </a>
-            </h6>
-            <ul class="nav flex-column mb-2">
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Current month
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Last quarter
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Social engagement
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Year-end sale
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <main class="mt-5">
-          <div class="container">
-            <div class="row mb-4">
-              <div class="col-md-6">
-                <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg" class="img-fluid shadow-5 rounded" alt="">
-              </div>
-              <div class="col-md-6">
-                <h1>Esta es la primera página</h1>
-                <hr>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nesciunt aperiam assumenda dolores expedita autem veritatis distinctio vitae, dignissimos optio vel, quidem corrupti corporis aspernatur a. Excepturi dolore itaque illo.
-                </p>
-              </div>
-            </div>
-
-            <div class="row mb-4">
-              <div class="col-lg-4 col-md-12">
-                <div class="card">
-                  <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img
-                      src="https://mdbootstrap.com/img/new/standard/nature/111.jpg"
-                      class="img-fluid"
-                    />
-                    <a href="#!">
-                      <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                  </div>
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and make up the bulk of the
-                      card's content.
-                    </p>
-                    <a href="#!" class="btn btn-primary">Button</a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-4 col-md-6">
-                <div class="card">
-                  <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img
-                      src="https://mdbootstrap.com/img/new/standard/nature/112.jpg"
-                      class="img-fluid"
-                    />
-                    <a href="#!">
-                      <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                  </div>
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and make up the bulk of the
-                      card's content.
-                    </p>
-                    <a href="#!" class="btn btn-primary">Button</a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-4 col-md-6">
-                <div class="card">
-                  <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                    <img
-                      src="https://mdbootstrap.com/img/new/standard/nature/113.jpg"
-                      class="img-fluid"
-                    />
-                    <a href="#!">
-                      <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                    </a>
-                  </div>
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and make up the bulk of the
-                      card's content.
-                    </p>
-                    <a href="#!" class="btn btn-primary">Button</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div><!-- Container -->
-        </main>
+    
+    <nav class="nav">
+      <div class="nav__links">
+        <a href="#" class="nav__link">
+          <i class="fa fa-user"></i>Dashboard
+        </a>
+        <a class="nav__link nav__link--active" href="#">
+          <i class="fa fa-user"></i>Projects
+        </a>
+        <a class="nav__link" href="#">
+          <i class="fa fa-user"></i>Security
+        </a>
+        <a class="nav__link" href="#">
+          <i class="fa fa-user"></i>History
+        </a>
+        <a class="nav__link" href="#">
+          <i class="fa fa-user"></i>Profile
+        </a>
       </div>
-    </div>
+      <div class="overlay"></div>
+    </nav>
+
+    <main class="main-content mt-5">
+      <div class="container">  
+        <div class="row mb-4">
+          <div class="col-md-6">
+            <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg" class="img-fluid shadow-5 rounded" alt="">
+          </div>
+          <div class="col-md-6">
+            <h1>Esta es la primera página</h1>
+            <hr>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nesciunt aperiam assumenda dolores expedita autem veritatis distinctio vitae, dignissimos optio vel, quidem corrupti corporis aspernatur a. Excepturi dolore itaque illo.
+            </p>
+          </div>
+        </div>
+      
+        <div class="row mb-4">
+          <div class="col-lg-4 col-md-12">
+            <div class="card">
+              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img
+                  src="https://mdbootstrap.com/img/new/standard/nature/111.jpg"
+                  class="img-fluid"
+                />
+                <a href="#!">
+                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up the bulk of the
+                  card's content.
+                </p>
+                <a href="#!" class="btn btn-primary">Button</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="card">
+              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img
+                  src="https://mdbootstrap.com/img/new/standard/nature/112.jpg"
+                  class="img-fluid"
+                />
+                <a href="#!">
+                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up the bulk of the
+                  card's content.
+                </p>
+                <a href="#!" class="btn btn-primary">Button</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="card">
+              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img
+                  src="https://mdbootstrap.com/img/new/standard/nature/113.jpg"
+                  class="img-fluid"
+                />
+                <a href="#!">
+                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up the bulk of the
+                  card's content.
+                </p>
+                <a href="#!" class="btn btn-primary">Button</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div><!-- Container -->
+    </main>
 
     @include('layouts.includes.templates._footer')
 
     <!-- JavaScript -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+    <!-- https://www.youtube.com/watch?v=yH7gkUNwDJY -->
     <script>
-      var menuBtn = document.querySelector('#menu-btn')
-      var sidebarMenu = document.querySelector('#sidebarMenu')
-      var container = document.querySelector(".my-container")
+      window.addEventListener("load", () => {
+        document.body.classList.remove("preload");
+      });
+      
+      document.addEventListener("DOMContentLoaded", () => {
+        const nav = document.querySelector(".nav");
 
-      menuBtn.addEventListener('click', () => {
-        sidebarMenu.classList.toggle('active-nav')
-        container.classList.toggle("active-cont")
-      })
+        document.querySelector("#btnNav").addEventListener("click", () => {
+          nav.classList.add("nav--open");
+        });
+
+        document.querySelector(".overlay").addEventListener("click", () => {
+          nav.classList.remove("nav--open");
+        });
+      });
     </script>
   </body>
 </html>
