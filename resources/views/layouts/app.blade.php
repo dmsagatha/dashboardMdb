e76e5219308c9d0be8fee9d06d9b4908c416550c
Doris Morales <dmorales55@gmail.com>
2021-02-18 16:03:32 -0500

    Configuracion del Sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,113 +16,16 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    <style>
-      .container-for-admin {
-        background-color: rgb(255, 255, 255) !important;
-      }
-
-      .container {
-        margin-top: 4rem;
-      }
-
-      .nav-link:hover {
-        color:white !important;
-      }
-
-      .bg-footer {
-        background-color: #0288D1;
-      }
-
-      .unique-color {
-        background-color: #3f729b !important;
-      }
-
-      .sidebar-fixed {
-        position: fixed;
-        height: 100vh;
-        width: 80%;
-        max-width: 300px;
-        left: -300px;
-        top: 0;
-        background: linear-gradient(to right, #673AB7, #5E35B1);
-        z-index: 1050;      /* Capa por encima */
-        background-color: #fff;
-        -webkit-transition: all 0.3s ease;
-                transition: all 0.3s ease;
-        -webkit-box-shadow: 0 2px 5px 0 rgba(0,0,0,.16),0 2px 10px 0 rgba(0,0,0,.12);
-                box-shadow: 0 2px 5px 0 rgba(0,0,0,.16),0 2px 10px 0 rgba(0,0,0,.12);
-      }
-
-      /* https://www.youtube.com/watch?v=DvpSKoCyN5Q - 5' */
-      /* https://akzhy.com/blog/sidenav-using-pure-css */
-      /* ====================================
-        Botón para desplegar el Menú lateral
-      ======================================= */
-      .toggle {
-        display: flex;
-        align-items: center;
-        padding-top: .3rem;
-        padding-bottom: .3rem;
-        margin-right: 1rem;
-        font-size: 2rem;
-        text-decoration: none;
-        white-space: nowrap;
-        color: #fff;
-        cursor: pointer;
-      }
-
-      .toggle:hover, .toggle:focus {
-        color: rgb(236, 225, 225);
-      }
-
-      /* =================================
-        Abrir/cerrar el navegador lateral
-        Capa por encima de toda la página
-      ==================================== */
-      .sideNav-overlay {
-        position: fixed;
-        width: 100vw;
-        height: 100vh;
-        background: rgba(0, 0, 0, 0.7);
-        left: 100%;
-        top: 0;
-        z-index: 998;
-        cursor: default;
-      }
-
-      .sidebar-fixed:target {
-        left: 0;
-      }
-
-      .sidebar-fixed:target+.sideNav-overlay {
-        left: 0;
-      }
-    </style>
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
   </head>
-  <body class="grey lighten-3">
+  <body>
     <div class="container-for-admin">
       <header>
         @include('layouts.includes.templates._header')
       </header>
-      
-      <div id="sideNav" class="sidebar-fixed position-fixed unique-color">
-        <div class="text-center">
-          <a class="logo-wrapper waves-effect" href="#">
-            <img src="{{ asset('img/carita-feliz.gif') }}" class="img-fluid" alt="" style="width: 25%;">
-          </a>
-        </div>
 
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Cras justo odio</li>
-          <li class="list-group-item">Dapibus ac facilisis in</li>
-          <li class="list-group-item">Morbi leo risus</li>
-          <li class="list-group-item">Porta ac consectetur ac</li>
-          <li class="list-group-item">Vestibulum at eros</li>
-        </ul>
-      </div>
-
-      <a href="#!" class="close sideNav-overlay"></a>
+      <!-- Sidebar  -->
+      @include('layouts.includes.templates._sideNav')
 
       <main>
         <div class="container">
---
+++ resources/views/layouts/includes/templates/_sideNav.blade.php
@@ -0,0 +1,17 @@
+<div id="sideNav" class="sidebar-fixed position-fixed unique-color">
+  <div class="text-center">
+    <a class="logo-wrapper waves-effect" href="#">
+      <img src="{{ asset('img/carita-feliz.gif') }}" class="img-fluid" alt="" style="width: 25%;">
+    </a>
+  </div>
+
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Morbi leo risus</li>
+    <li class="list-group-item">Porta ac consectetur ac</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+  </ul>
+</div>
+
+<a href="#!" class="close sideNav-overlay"></a>
